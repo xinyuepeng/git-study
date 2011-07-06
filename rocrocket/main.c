58df422055bdae7a4d1b29382328d89cf0e6b26e
Xin YuePeng <bugboy_bugboy@hotmail.com>
2011-07-06 16:08:04 +0800

    add time function
    display local time.

--- rocrocket/main.c
+++ rocrocket/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-  printf("Version: 0.01\n");
+  time_t mytime;
+  struct tm *mylocaltime;
+  
+  mytime = time(NULL);
+  mylocaltime = localtime(&mytime);
+  printf("Year: %d\n", mylocaltime->tm_year+1900);
+  printf("Month: %d\n", mylocaltime->tm_mon+1);
+  printf("Day: %d\n", mylocaltime->tm_mday);
+  printf("Hour: %d\n", mylocaltime->tm_hour);
+  printf("Min: %d\n", mylocaltime->tm_min);
+  printf("Second: %d\n", mylocaltime->tm_sec);
+  printf("Version: 0.02\n");
   printf("hello world!\n");
   return 0;
 }
